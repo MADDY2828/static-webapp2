ea54d084dd2d33401edaf9b3d12aa63612688618
MADDY2828 <madhubabuvadlamudi1@gmail.com>
2025-01-25 12:44:17 +0530

    fourth commit

--- js/index.js
+++ js/index.js
@@ -7,7 +7,7 @@ function HelloWorld(id) {
   greeting = document.createElement('h3');
   greeting.setAttribute('id', 'hello');
   greeting.style.visibility = "hidden";
-  greeting.innerHTML = "Hello,Welcome to CI/CD pipeline Good Morning";
+  greeting.innerHTML = "This is CI/CD Pipeline";
 
   element.appendChild(greeting);
 
